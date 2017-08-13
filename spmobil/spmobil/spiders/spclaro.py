459a4e7b075c88a8b0b6f81e32e8de1b87f14e44
Enrique Vicente <enrique@crehana.com>
2017-08-12 19:50:58 -0500

    set items

--- spmobil/spmobil/spiders/spclaro.py
+++ spmobil/spmobil/spiders/spclaro.py
@@ -10,9 +10,15 @@ class SpclaroSpider(CrawlSpider):
     allowed_domains = ['catalogo.claro.com.pe']
     start_urls = ['http://catalogo.claro.com.pe/catalogo/personas/renovacion/acuerdo-18/celulares/todos/todos']
 
-    #rules = (
-    #    Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    #)
+    def start_requests(self):
+        # https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#cookies-debug
+        # Multiple cookie sessions per spider
+        for i,url in enumerate(self.start_urls):
+            yield scrapy.Request(url,
+                                 self.parse,
+                                 method='POST',
+                                 meta={'cookiejar': i}
+                                 )
 
     def parse(self,response):
         meta = response.xpath('//div[@class="box-producto-in"]/a/@href')
@@ -20,7 +26,8 @@ class SpclaroSpider(CrawlSpider):
             url = urlparse.urljoin(response.url, m.extract())
             # Seleccionamos la direccion
             yield scrapy.Request(url,
-                                 self.parse_plan
+                                 self.parse_plan,
+                                 meta={'cookiejar': response.meta['cookiejar']}
                                  )
 
     def parse_plan(self, response):
@@ -35,8 +42,42 @@ class SpclaroSpider(CrawlSpider):
             val=r.xpath("//span").pop().text
             arr_esp.append({"nombre":nomb,"valor":val})
 
+        data1 = response.xpath('//input[@id="item_id"]/@value').extract()
+        for op1 in response.xpath('//select[@id="listas_id"]/option/@value'):
+            data2 = op1.extract()
+            for op2 in response.xpath('//select[@id="cuotas_id"]/option/@value'):
+                data3 = op2.extract()
+                #for plan in response.xpath('//label[@class="radioCustom_label"]'):
+                for plan in response.xpath('//div[@class="box-planes option_radio"]'):
+                    data4_body = html.fromstring(plan.extract())
+                    data4 = data4_body.xpath("//input/@value")[0]
+                    prec_plan = data4_body.xpath("//label/div/span").pop().text
+                    nomb_plan = data4_body.xpath("//label/div/h3").pop().text
+                    arr_carc = []
+                    for item_plan in data4_body.xpath("//label/ul/li/span"):
+                        arr_carc.append({item_plan.text:""})
+
+                    for cont,item_plan in enumerate(data4_body.xpath("//label/ul/li/p")):
+                        arr_carc[cont][arr_carc[cont].keys()[0]] = item_plan.text
+
+                    url = urlparse.urljoin("http://catalogo.claro.com.pe", "combo-precios.php?data1="+data1[0]+
+                                                         "&data2="+data2+"&data3="+data3+
+                                                         "&data4="+data4)
+                    yield scrapy.Request(url,
+                                         self.parse_item,
+                                         meta={'v_marca': marca,
+                                               'v_nombre': nombre,
+                                               'v_descripcion': descripcion,
+                                               'v_arr_esp': arr_esp,
+                                               'v_prec_plan': prec_plan,
+                                               'v_nomb_plan': nomb_plan,
+                                               'v_arr_carc_plan': arr_carc,
+                                               'cookiejar': response.meta['cookiejar']
+                                               }
+                                         )
 
     def parse_item(self, response):
+        print response._body
         i = {}
         #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
         #i['name'] = response.xpath('//div[@id="name"]').extract()
